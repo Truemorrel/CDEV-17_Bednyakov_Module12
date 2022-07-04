ce1d230695cb38d2f91ae8ebf5c5ed2d25bff2db
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-04 23:15:03 +0300

    master: add task 12.1.3.

---
+++ Task_12_1_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_12_1_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            var count = Convert.ToInt32(Console.ReadLine());
+            string[] array = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Все элементы записаны.");
+        }
+    }
+}
