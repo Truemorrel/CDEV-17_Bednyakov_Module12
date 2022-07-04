89e2bda278c53a1132fc7823eebf6822b1ea3fb4
Sergey Bednyakov <sbednyakov@gmail.com>
2022-07-04 20:54:23 +0300

    Add project files.

---
+++ Task_12_1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_12_1_2
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            var name = Console.ReadLine();
+            string greetings = $"Привет, " + name;
+            Console.WriteLine(greetings);
+
+        }
+    }
+}
